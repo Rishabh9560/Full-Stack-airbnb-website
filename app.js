c9c5b9598dc8f825c7f6ced774ba1dbfe0a12baa
Rishabh <rishabhrawat0987654@gamil.com>
2025-07-30 23:10:17 +0530

    new commit

---
+++ app.js
@@ -0,0 +1,109 @@
+const express = require("express") ;
+const port = 8080 ; 
+const mongoose = require("mongoose");
+const app = express() ;
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate")
+const wrapAsync = require("./utils/wrapAsync.js") ;
+const ExpressError = require("./utils/expressError.js") ;
+
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.engine('ejs' , ejsMate);
+app.set("view engine" , "ejs");
+app.set("views" , path.join(__dirname , "views"));
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main()
+.then(() =>{
+    console.log("Connected to DB") ;
+ })
+ .catch((err) =>{
+    console.log(err)
+ });
+async function main(){
+    await mongoose.connect(MONGO_URL) ;
+}
+// app.get("/testListing" ,async (req ,res) => {
+//     let sampleListing = new Listing({
+//         title : "My New Villa" , 
+//         description : "By the Beach",
+//         price : 1200 ,
+//         location : "Calanguate , Goa" ,
+//         country: "India" ,
+
+//     });
+//     await sampleListing.save();
+//     console.log("Sample was saved") ;
+
+
+// })
+app.get("/" , (req  , res) => {
+    res.send("hi , I am a root") ;
+});
+//------------------INDEX ROUTE-----------------//
+app.get("/listings" ,async (req , res) =>{
+    const allListings = await Listing.find({}) ;
+    res.render("listings/index.ejs" , {allListings})
+});
+//-----------NEW ROUTE------------//
+app.get("/listings/new" , (req  ,res) =>{
+    res.render("listings/new.ejs");
+});
+//------------SHOW ROUTE-------------//
+app.get("/listings/:id" ,async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs" , {listing})
+
+});
+//----------create new route---//
+app.post("/listings" , async (req , res , next) =>{
+    
+    const newListings = new Listing(req.body.listing);
+    await newListings.save();
+    res.redirect("/listings") ;
+   
+});
+//---------EDIT ROUTE-------//
+app.get("/listings/:id/edit" , async (req,res)=>{
+    let {id} =req.params;
+    const listing = await Listing.findById(id) ;
+    res.render("listings/edit.ejs", {listing});
+});
+//--------------UPDATE ROUTE--------//
+app.put("/listings/:id", async (req,res)=>{
+    let {id} = req.params ;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect("/listings");
+});
+//--------------DELETE ROUTE--------------//
+app.delete("/listings/:id" ,  async(req  , res)=>{
+    let {id} = req.params ;
+    let deletedListing = await Listing.findByIdAndDelete(id) ;
+    res.redirect("/listings");
+
+});
+
+//--------------------ERROR HANDLING-------//
+
+// app.all("*" , ( req ,res , next)=>{
+//     next(new expressError(404 , "page not found"));
+
+// }) ;
+// app.use((err,req , res , next)=>{
+//     let {statuscode , message} = err ;
+//     res.status(statuscode).send(message) ;
+
+
+
+// });
+ 
+app.listen((port) ,() =>{
+    console.log("app listening to port 8080");
+})
---
+++ models/review.js
@@ -0,0 +1,18 @@
+const mangoose = require("mongoose") ;
+const Schema  = mongoose.Schema ; 
+
+
+const reviewSchema  = new Schema({
+    comment : String ,
+     rating :{
+        type :number , 
+        min : 1 , 
+        max: 5 ,
+     },
+     createdAt :{
+        type: Date()  , 
+        default : Date.now() ,
+
+     }
+});
+module.exports = mongoose.model("Review" , reviewSchema) ;
